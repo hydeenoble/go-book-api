d5631dc9231b6da4ee6352a4cf031ab23753b6fd
Emehinola Idowu <hydeenoble39@gmail.com>
2019-05-27 18:17:24 +0100

    Added an implemtation for the update functions

--- main.go
+++ main.go
@@ -59,7 +59,20 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 
 // Update a Book
 func updateBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = strconv.Itoa(rand.Intn(10000000))
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 // Delete a Book
 func deleteBook(w http.ResponseWriter, r *http.Request) {
