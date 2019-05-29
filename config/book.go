83ef5f717bbcedd75e670c0adbfde82bda2cc06e
Emehinola Idowu <hydeenoble39@gmail.com>
2019-05-29 17:27:22 +0100

    Changed postgres db to mongo db because I dont have the strength to write SQL queries :)

--- config/book.go
+++ config/book.go
@@ -8,8 +8,8 @@ import (
 )
 
 type Config struct {
-	API      *APIConfig
-	Postgres *PostgresConfig
+	API   *APIConfig
+	Mongo *MongoConfig
 }
 
 type APIConfig struct {
@@ -17,7 +17,7 @@ type APIConfig struct {
 	Port int
 }
 
-type PostgresConfig struct {
+type MongoConfig struct {
 	Username string
 	Password string
 	Host     string
@@ -27,14 +27,14 @@ type PostgresConfig struct {
 
 func GetConfig() *Config {
 	port, portErr := strconv.Atoi(os.Getenv("PORT"))
-	postgresPort, postgresPortErr := strconv.Atoi(os.Getenv("POSTGRES_PORT"))
+	mongoPort, mongoPortErr := strconv.Atoi(os.Getenv("MONGO_PORT"))
 
 	if portErr != nil {
 		log.Fatal(portErr)
 	}
 
-	if postgresPortErr != nil {
-		log.Fatal(postgresPortErr)
+	if mongoPortErr != nil {
+		log.Fatal(mongoPortErr)
 	}
 
 	return &Config{
@@ -42,12 +42,12 @@ func GetConfig() *Config {
 			Name: "Sample Book API In Golang",
 			Port: port,
 		},
-		Postgres: &PostgresConfig{
-			Username: os.Getenv("POSTGRES_USER"),
-			Password: os.Getenv("POSTGRES_PASSWORD"),
-			Host:     os.Getenv("POSTGRES_HOST"),
-			DbName:   os.Getenv("POSTGRES_DB"),
-			Port:     postgresPort,
+		Mongo: &MongoConfig{
+			Username: os.Getenv("MONGO_USER"),
+			Password: os.Getenv("MONGO_PASSWORD"),
+			Host:     os.Getenv("MONGO_HOST"),
+			DbName:   os.Getenv("MONGO_DB"),
+			Port:     mongoPort,
 		},
 	}
 }
--- model/db.go
+++ model/db.go
@@ -1,34 +1,51 @@
 package model
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/hydeenoble/mux-rest-api/config"
 	_ "github.com/lib/pq"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-// var db
+// var db *sql.DB
+// var err error
+
 var (
-	host     = config.GetConfig().Postgres.Host
-	port     = config.GetConfig().Postgres.Port
-	user     = config.GetConfig().Postgres.Username
-	password = config.GetConfig().Postgres.Password
-	dbname   = config.GetConfig().Postgres.DbName
+	host     = config.GetConfig().Mongo.Host
+	port     = config.GetConfig().Mongo.Port
+	user     = config.GetConfig().Mongo.Username
+	password = config.GetConfig().Mongo.Password
+	dbname   = config.GetConfig().Mongo.DbName
 )
 
 func init() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	defer db.Close()
+	ctx, cancel = context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	err = client.Ping(ctx, readpref.Primary())
 
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
+	// psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	// db, err = sql.Open("postgres", psqlInfo)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer db.Close()
+
+	// err = db.Ping()
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	fmt.Println("Successfully connected!")
 }
