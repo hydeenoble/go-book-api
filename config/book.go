7419b9090e893a6cd16e033212323ca7c780eb64
Emehinola Idowu <hydeenoble39@gmail.com>
2019-05-29 15:56:06 +0100

    Added configurations for Postgres DB

--- config/book.go
+++ config/book.go
@@ -7,7 +7,8 @@ import (
 )
 
 type Config struct {
-	API *APIConfig
+	API      *APIConfig
+	Postgres *PostgresConfig
 }
 
 type APIConfig struct {
@@ -15,15 +16,41 @@ type APIConfig struct {
 	Port int
 }
 
+type PostgresConfig struct {
+	Username string
+	Password string
+	Host     string
+	DbName   string
+	Port     int
+}
+
 func GetConfig() *Config {
 	port, portErr := strconv.Atoi(os.Getenv("PORT"))
+	postgresUsername := os.Getenv("POSTGRES_USER")
+	postgresPassword := os.Getenv("POSTGRES_PASSWORD")
+	postgresHost := os.Getenv("POSTGRES_HOST")
+	postgresDbName := os.Getenv("POSTGRES_DB")
+	postgresPort, postgresPortErr := strconv.Atoi(os.Getenv("POSTGRES_PORT"))
+
 	if portErr != nil {
 		log.Fatal(portErr)
 	}
+
+	if postgresPortErr != nil {
+		log.Fatal(postgresPortErr)
+	}
+
 	return &Config{
 		API: &APIConfig{
 			Name: "Sample Book API In Golang",
 			Port: port,
+		},
+		Postgres: &PostgresConfig{
+			Username: postgresUsername,
+			Password: postgresPassword,
+			Host:     postgresHost,
+			DbName:   postgresDbName,
+			Port:     postgresPort,
 		},
 	}
 }
