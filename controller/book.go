9a20a3e8b9fc7862d61d47176766dacc7a7b82dc
Emehinola Idowu <hydeenoble39@gmail.com>
2019-05-31 09:27:36 +0100

    Fix bug with getting defined 'id' in mongo DB

--- controller/book.go
+++ controller/book.go
@@ -2,35 +2,22 @@ package controller
 
 import (
 	"encoding/json"
-	"math/rand"
+	// "math/rand"
 	"net/http"
-	"strconv"
+	// "strconv"
 
-	"github.com/gorilla/mux"
+	// "github.com/gorilla/mux"
 	"github.com/hydeenoble/mux-rest-api/model"
+	"github.com/hydeenoble/mux-rest-api/schema"
 )
-
-// Book Struct (Model)
-type Book struct {
-	ID     string  `json:"id"`
-	Isbn   string  `json:"isbn"`
-	Title  string  `json:"title"`
-	Author *Author `json:"author"`
-}
-
-// Author Struct
-type Author struct {
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
-}
 
 // Init books var as a slice Book Struct
 
-var books []Book
+var books []schema.Book
 
 func init() {
-	books = append(books, Book{ID: "1", Isbn: "448743", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
-	books = append(books, Book{ID: "2", Isbn: "433323", Title: "Book Two", Author: &Author{Firstname: "Steve", Lastname: "Smith"}})
+	// books = append(books, Book{ID: "1", Isbn: "448743", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
+	// books = append(books, Book{ID: "2", Isbn: "433323", Title: "Book Two", Author: &Author{Firstname: "Steve", Lastname: "Smith"}})
 }
 
 // Get All Books
@@ -41,53 +28,53 @@ func GetBooks(w http.ResponseWriter, r *http.Request) {
 
 // Get single Book
 func GetBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r) // Get Params
-	// Loop through books and find with ID
-	for _, book := range books {
-		if book.ID == params["id"] {
-			json.NewEncoder(w).Encode(book)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(&Book{})
+	// w.Header().Set("Content-Type", "application/json")
+	// params := mux.Vars(r) // Get Params
+	// // Loop through books and find with ID
+	// for _, book := range books {
+	// 	if book.ID == params["id"] {
+	// 		json.NewEncoder(w).Encode(book)
+	// 		return
+	// 	}
+	// }
+	// json.NewEncoder(w).Encode(&Book{})
 }
 
 //Create a New Book
 func CreateBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var book model.Book
+	var book schema.Book
 	_ = json.NewDecoder(r.Body).Decode(&book)
 	json.NewEncoder(w).Encode(model.CreateBook(book))
 }
 
 // Update a Book
 func UpdateBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range books {
-		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
-			var book Book
-			_ = json.NewDecoder(r.Body).Decode(&book)
-			book.ID = strconv.Itoa(rand.Intn(10000000))
-			books = append(books, book)
-			json.NewEncoder(w).Encode(book)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(books)
+	// w.Header().Set("Content-Type", "application/json")
+	// params := mux.Vars(r)
+	// for index, item := range books {
+	// 	if item.ID == params["id"] {
+	// 		books = append(books[:index], books[index+1:]...)
+	// 		var book Book
+	// 		_ = json.NewDecoder(r.Body).Decode(&book)
+	// 		book.ID = strconv.Itoa(rand.Intn(10000000))
+	// 		books = append(books, book)
+	// 		json.NewEncoder(w).Encode(book)
+	// 		return
+	// 	}
+	// }
+	// json.NewEncoder(w).Encode(books)
 }
 
 // Delete a Book
 func DeleteBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range books {
-		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
-			break
-		}
-	}
-	json.NewEncoder(w).Encode(books)
+	// w.Header().Set("Content-Type", "application/json")
+	// params := mux.Vars(r)
+	// for index, item := range books {
+	// 	if item.ID == params["id"] {
+	// 		books = append(books[:index], books[index+1:]...)
+	// 		break
+	// 	}
+	// }
+	// json.NewEncoder(w).Encode(books)
 }
--- model/book.go
+++ model/book.go
@@ -5,29 +5,16 @@ import (
 	"log"
 	"time"
 
+	"github.com/hydeenoble/mux-rest-api/schema"
 	"go.mongodb.org/mongo-driver/bson"
 )
-
-// Book Struct (Model)
-type Book struct {
-	ID     string  `json:"id"`
-	Isbn   string  `json:"isbn"`
-	Title  string  `json:"title"`
-	Author *Author `json:"author"`
-}
-
-// Author Struct
-type Author struct {
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
-}
 
 func init() {
 }
 
-func GetBooks() []*Book {
+func GetBooks() []*schema.Book {
 	// books := Book{ID: "1", Isbn: "448743", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}}
-	var books []*Book
+	var books []*schema.Book
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	res, err := booksCollection.Find(ctx, bson.D{{}})
@@ -40,7 +27,7 @@ func GetBooks() []*Book {
 	for res.Next(context.Background()) {
 
 		// create a value into which the single document can be decoded
-		var book Book
+		var book schema.Book
 		err := res.Decode(&book)
 		if err != nil {
 			log.Fatal(err)
@@ -59,7 +46,7 @@ func GetBooks() []*Book {
 	return books
 }
 
-func CreateBook(book Book) Book {
+func CreateBook(book schema.Book) schema.Book {
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	_, err := booksCollection.InsertOne(ctx, book)
 	if err != nil {
